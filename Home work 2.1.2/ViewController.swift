ef325110723b8cfe97ded339e8dcb687ec4f1caf
ValeriyHub <p04taera@gmail.com>
2021-04-14 20:54:00 +0300

    Initial Commit

---
+++ Home work 2.1.2/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Home work 2.1.2
+//
+//  Created by Valerii D on 14.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var redSignal: UIView!
+    @IBOutlet var yellowSignal: UIView!
+    @IBOutlet var greenSignal: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var resetButton: UIButton!
+    
+    var redTurn = true
+    var greenTurn = false
+    var yellowTurn = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redSignal.alpha = 0.3
+        redSignal.layer.cornerRadius = 52
+        
+        yellowSignal.alpha = 0.3
+        yellowSignal.layer.cornerRadius = 52
+        
+        greenSignal.alpha = 0.3
+        greenSignal.layer.cornerRadius = 52
+        
+        startButton.layer.cornerRadius = 10
+        resetButton.layer.cornerRadius = 8
+    }
+
+    @IBAction func changeCollorButton() {
+        if redTurn && !yellowTurn && !greenTurn {
+            redSignal.alpha = 1
+            yellowSignal.alpha = 0.3
+            greenSignal.alpha = 0.3
+            redTurn = false
+            yellowTurn = true
+        } else if !redTurn && yellowTurn && !greenTurn {
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 1
+            greenSignal.alpha = 0.3
+            yellowTurn = false
+            greenTurn = true
+        } else if !redTurn && !yellowTurn && greenTurn {
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 0.3
+            greenSignal.alpha = 1
+            greenTurn = false
+            redTurn = true
+        }
+    }
+    
+    @IBAction func reset() {
+        redSignal.alpha = 0.3
+        yellowSignal.alpha = 0.3
+        greenSignal.alpha = 0.3
+        
+        redTurn = true
+        greenTurn = false
+        yellowTurn = false
+    }
+}
+
